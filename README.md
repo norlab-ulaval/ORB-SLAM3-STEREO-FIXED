4d0e6246f35a0e84eb1505ef96f7a9c8fa081d2a
boxanm <matej.boxan@gmail.com>
2026-02-15 22:04:32 -0500

    Fix build

--- README.md
+++ README.md
@@ -26,6 +26,6 @@ This will create **libORB_SLAM3.so**  at *lib* folder and the executables in *Ex
 
 Run Docker
 ```
-docker build -f .devcontainer/Dockerfile -t orbslam3Offline .
-docker run -it --rm -v $(pwd):/workspaces/orbslam3 orbslam3Offline:latest run_orbslam3
+docker build -f .devcontainer/Dockerfile -t orbslam3offline .
+docker run -it --rm -v $(pwd):/workspaces/orbslam3 orbslam3offline:latest run_orbslam3
 ```
--- build.sh
+++ build.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
+cd Thirdparty/DBoW2
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
 
-mkdir build
+cd ../../g2o
+
+echo "Configuring and building Thirdparty/g2o ..."
+
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+cd ../../Sophus
+
+echo "Configuring and building Thirdparty/Sophus ..."
+
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+
+cd ../../../
+
+mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 make -j10
